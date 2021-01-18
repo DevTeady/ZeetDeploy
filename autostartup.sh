12db0a4a9dc9477869eb8626dbe64cb07cb2e8be
Mudabbirul Islam <71183979+MudabbirulSaad@users.noreply.github.com>
2021-01-18 18:08:07 +0600

    Update autostartup.sh

--- autostartup.sh
+++ autostartup.sh
@@ -12,7 +12,7 @@ fi
 #CREATE USERS.
 # username:passsword:Y
 # username2:password2:Y
-echo "music:music:Y" >/root/createusers.txt
+echo "saad:8775:Y" >/root/createusers.txt
 file="/root/createusers.txt"
 if [ -f $file ]
   then
